158d2734591687fe07732755362d2875b7119af7
Franklin Junior <franklinjunior12@hotmail.com>
2025-08-22 10:41:39 -0300

    Add substring search, replacement, and length computation to string handling

--- strings.cbl
+++ strings.cbl
@@ -7,6 +7,7 @@
        01 LAST-NAME PIC X(20) VALUE "Smith".
        01 FULL-NAME PIC X(20).
        01 STRING-POINTER PIC 99 VALUE 5.
+       01 STR-COUNT PIC 99.
 
        PROCEDURE DIVISION.
            DISPLAY "Starting main program".
@@ -31,5 +32,18 @@
            UNSTRING FULL-NAME DELIMITED BY " "
                INTO FIRST-NAME, LAST-NAME.
            DISPLAY FIRST-NAME LAST-NAME.
+           *> find if string contains substring
+           INSPECT FULL-NAME TALLYING STR-COUNT FOR ALL "John".
+           IF STR-COUNT > 0 THEN
+               DISPLAY "Name contains John"
+           ELSE
+               DISPLAY "Name does not contains John"
+           END-IF.
+           *> replace string
+           INSPECT FULL-NAME REPLACING ALL "John" BY "Carl".
+           DISPLAY FULL-NAME.
+           *> size of string
+           COMPUTE STR-COUNT = FUNCTION LENGTH(FULL-NAME).
+           DISPLAY "Size of string " STR-COUNT.
            DISPLAY "Ending main program".
            STOP RUN.
